86d206d763ad15f83e898992d2b6f37e55caaee4
rana118803 <rananasser118803@gmail.com>
2021-05-23 19:15:34 +0200

    View Key words && add Key word

--- RECO/Forms/KeyWords.Designer.cs
+++ RECO/Forms/KeyWords.Designer.cs
@@ -30,6 +30,7 @@ namespace RECO.Forms
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.Add = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panel1
@@ -40,11 +41,22 @@ namespace RECO.Forms
             this.panel1.Size = new System.Drawing.Size(400, 414);
             this.panel1.TabIndex = 1;
             // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(286, 469);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(126, 42);
+            this.Add.TabIndex = 0;
+            this.Add.Text = "Add Keyword +";
+            this.Add.UseVisualStyleBackColor = true;
+           // this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
             // KeyWords
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1063, 475);
+            this.ClientSize = new System.Drawing.Size(1063, 518);
+            this.Controls.Add(this.Add);
             this.Controls.Add(this.panel1);
             this.Name = "KeyWords";
             this.Text = "KeyWords";
@@ -55,6 +67,7 @@ namespace RECO.Forms
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Add;
         //private System.Windows.Forms.Button button1;
     }
 }
--- RECO/Forms/KeyWords.cs
+++ RECO/Forms/KeyWords.cs
@@ -17,6 +17,8 @@ namespace RECO.Forms
         {
             InitializeComponent();
             viewKeywords(dirPath);
+            
+            
         }
 
         public void viewKeywords(string dirPath)
@@ -64,36 +66,40 @@ namespace RECO.Forms
                 Directory.CreateDirectory(dir);
                 viewKeywords(dirPath);
             }
-        
 
-            // 
-            // addButton
-            // 
-           
-           
-
-
-
+            Add.Click += delegate
+            {
+                Directory.CreateDirectory(dir);
+                viewKeywords(dirPath);
+            };
 
 
         }
 
-        public void addKeywords()
-        {
-            // 
-            // addButton
-            // 
-            Button addButton = new Button();
-            addButton.Location = new System.Drawing.Point(254, 9);
-            addButton.Name = "addButton";
-            addButton.Size = new System.Drawing.Size(47, 36);
-            addButton.TabIndex = 1;
-            addButton.Text = "+";
-            addButton.UseVisualStyleBackColor = true;
+       
+        //public void Add_Click(object sender, EventArgs e)
+        //{
+        //    // 
+        //    // Add
+        //    // 
+        //    int directoryCount = System.IO.Directory.GetDirectories(dirPath).Length;
+        //    int Number = directoryCount;
+        //    int firstRepo = Number + 1;
+        //    string createdName = @"\New Repo " + firstRepo;
+        //    string dir = dirPath + createdName;
+        //    string[] dirs = System.IO.Directory.GetDirectories(dirPath); // add all dirs in an array to check if it empty or not
+        //    DirectoryInfo di = new DirectoryInfo(dirPath); // get all info
+        //                                                   // Get a reference to each directory in that directory
+        //    DirectoryInfo[] dirArr = di.GetDirectories();
 
-            panel1.Controls.Add(addButton);
-        }
-
-      
+        //    Button Add = new Button();
+        //    this.Add.Location = new System.Drawing.Point(286, 469);
+        //    this.Add.Name = "Add";
+        //    this.Add.Size = new System.Drawing.Size(126, 42);
+        //    this.Add.TabIndex = 0;
+        //    this.Add.Text = "Add Keyword +";
+        //    this.Add.UseVisualStyleBackColor = true;
+        //    panel1.Controls.Add(Add);
+        //}
     }
 }
